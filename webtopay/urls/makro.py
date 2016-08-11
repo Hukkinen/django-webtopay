e7257d4b4138068e755ca5d0c87c092b59f469f1
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2016-08-11 15:43:58 +0300

    Removed south migration, added core migrations, move urls to 1.9x style

--- webtopay/urls/makro.py
+++ webtopay/urls/makro.py
@@ -1,11 +1,6 @@
-import django
+from django.conf.urls import url
+from webtopay.views import makro
 
-if django.VERSION >= (1, 6):
-    from django.conf.urls import patterns, url
-else:
-    from django.conf.urls.defaults import patterns, url
-
-
-urlpatterns = patterns('webtopay.views',
-    url(r'^$', 'makro', name="webtopay-makro"),
-)
+urlpatterns = [
+    url(r'^$', makro, name="webtopay-makro")
+]
--- webtopay/urls/mikro.py
+++ webtopay/urls/mikro.py
@@ -1,11 +1,6 @@
-import django
+from django.conf.urls import url
+from webtopay.views import mikro
 
-if django.VERSION >= (1, 6):
-    from django.conf.urls import patterns, url
-else:
-    from django.conf.urls.defaults import patterns, url
-
-
-urlpatterns = patterns('webtopay.views',
-    url(r'^$', 'mikro', name="webtopay-mikro"),
-)
+urlpatterns = [
+    url(r'^$', mikro, name="webtopay-mikro")
+]
